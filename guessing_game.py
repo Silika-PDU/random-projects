b30084f5f34202332c5a1ada27de94cab11dd487
Mustafa Kashif <silika.pdu@gmail.com>
2026-07-13 18:54:25 +0500

    Number Guessing Game

---
+++ guessing_game.py
@@ -0,0 +1,15 @@
+import random
+
+guess_number = random.randint(1, 100)
+
+user_guess = 0  # Initialize the variable
+
+while user_guess != guess_number:
+    user_guess = int(input("Guess the number: "))
+
+    if user_guess > guess_number:
+        print("Lower")
+    elif user_guess < guess_number:
+        print("Higher")
+    else:
+        print("Correct guess!")
