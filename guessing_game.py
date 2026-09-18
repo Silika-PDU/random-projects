1f6c7f8e5027e6ae916e6b9334e8290f8c2b39ff
Mustafa Kashif <silika.pdu@gmail.com>
2026-09-18 05:20:52 +0200

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,7 +2,7 @@ import random
 
 guess_number = random.randint(1, 100)
 
-user_guess = 0  # Initialize the variable
+user_guess = 0  
 
 while user_guess != guess_number:
     user_guess = int(input("Guess the number: "))
